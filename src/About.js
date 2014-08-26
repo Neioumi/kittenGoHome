bcc93a1b84830ce92e1893b24bfb70a833089e9d
Neioumi <neioumi@me.com>
2014-08-26 23:56:01 +0900

    Added copyright notice of materials

--- src/About.js
+++ src/About.js
@@ -4,14 +4,23 @@ var About = cc.Layer.extend({
         var winSize = cc.Director.getInstance().getWinSize();
 
         var menuLabel = cc.LabelTTF.create("About", "Arial", 38);
-        menuLabel.setPosition(cc.p(winSize.width / 2, winSize.height / 2 + 100));
+        menuLabel.setPosition(cc.p(winSize.width / 2, winSize.height / 2 + 360));
         this.addChild(menuLabel, 5);
 
+        var iconsCopyright = cc.LabelTTF.create("Icons made by Elegant Themes, \n Freepik from Flaticon. \n http://www.flaticon.com", "Arial", 26, cc.size(winSize.width * 0.85, 100), cc.TEXT_ALIGNMENT_CENTER);
+        iconsCopyright.setPosition(winSize.width / 2, winSize.height / 2 + 100);
+        iconsCopyright.setAnchorPoint(0.5, 0.5);
+        this.addChild(iconsCopyright);
+
+        var musicCopyright = cc.LabelTTF.create("Music from 魔王魂. \n http://maoudamashii.jokersounds.com", "Arial", 26, cc.size(winSize.width * 0.85, 80), cc.TEXT_ALIGNMENT_CENTER);
+        musicCopyright.setPosition(winSize.width / 2, winSize.height / 2 - 30);
+        musicCopyright.setAnchorPoint(0.5, 0.5);
+        this.addChild(musicCopyright);
+
         var backBtn = cc.Sprite.create(res.s_SpriteBtn, cc.rect(0, 60, 222, 60));
-        backBtn.setAnchorPoint(0, 0);
+        backBtn.setAnchorPoint(cc.p(0.5, 0.5));
         backBtn.setPosition(0, 0);
         var back = cc.MenuItemLabel.create(backBtn, this.onBackCallback);
-        // this.addChild(backBtn);
 
         var menu = cc.Menu.create(back);
         menu.alignItemsVerticallyWithPadding(20);
@@ -26,14 +35,14 @@ var About = cc.Layer.extend({
         scene.addChild(Menu.create());
         cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
     },
-    onTouchesBegan:function (touches, event) {
-    },
-    onTouchesMoved:function (touches, event) {
-    },
-    onTouchesEnded:function (touches, event) {
-    },
-    onTouchesCancelled:function (touches, event) {
-    }
+    // onTouchesBegan:function (touches, event) {
+    // },
+    // onTouchesMoved:function (touches, event) {
+    // },
+    // onTouchesEnded:function (touches, event) {
+    // },
+    // onTouchesCancelled:function (touches, event) {
+    // }
 });
 
 About.create = function () {
